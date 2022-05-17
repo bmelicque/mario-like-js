4c92e5c7a5c6b769b7bfdf27f78816ca1c6b6c22
bmelicque <bmelicque@gmail.com>
2022-05-17 15:52:31 +0200

    world scaling w/ css

--- script.js
+++ script.js
@@ -6,26 +6,6 @@ import Inputs from "./lib/models/inputs.js";
 export const WORLD_WIDTH = 100;
 export const WORLD_HEIGHT = 55;
 
-const worldElement = document.querySelector("[data-world]");
-
-setPixelToWorldScale();
-window.addEventListener("resize", setPixelToWorldScale);
-
-function setPixelToWorldScale() {
-	let worldToPixelScale;
-	if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT)
-		worldToPixelScale = window.innerWidth / WORLD_WIDTH;
-	else worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
-
-	worldElement.style.setProperty(
-		"--world-to-pixel-scale",
-		worldToPixelScale + "px"
-	);
-
-	worldElement.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
-	worldElement.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
-}
-
 const inputs = new Inputs();
 inputs.addEventListeners();
 
